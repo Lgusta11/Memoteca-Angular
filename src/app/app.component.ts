16a66b8f413613cb67ddff59ef382af7241ab077
Lgusta11 <rainlima2020@gmail.com>
2024-08-13 23:17:56 -0300

    introdução angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./Components/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./Components/rodape/rodape.component";
+import { CriarPensamentosComponent } from "./Components/pensamentos/criar-pensamentos/criar-pensamentos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
